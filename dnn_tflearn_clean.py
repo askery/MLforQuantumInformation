143739a078a7e52acd7fed9b1685c0d92936f4a8
askery <askery@gmail.com>
2018-02-27 13:22:39 -0300

    DNN with N layers with M neurons to compare with sklearn NN

--- dnn_tflearn_clean.py
+++ dnn_tflearn_clean.py
@@ -63,23 +63,14 @@ pca.fit(X)
 X =  pca.transform(X)
 testX =  pca.transform(testX)
 
-# Building deep neural network
-input_layer = tflearn.input_data(shape=[None, 32])
-dense1 = tflearn.fully_connected(input_layer, 64, activation='tanh',
-                                 regularizer='L2', weight_decay=0.001)
-dropout1 = tflearn.dropout(dense1, 0.8)
-dense2 = tflearn.fully_connected(dropout1, 64, activation='tanh',
-                                 regularizer='L2', weight_decay=0.001)
-dropout2 = tflearn.dropout(dense2, 0.8)
-softmax = tflearn.fully_connected(dropout2, 2, activation='softmax')
-
-# Regression using SGD with learning rate decay and Top-3 accuracy
-sgd = tflearn.SGD(learning_rate=0.1, lr_decay=0.96, decay_step=1000)
-#top_k = tflearn.metrics.Top_k(3)
-net = tflearn.regression(softmax, optimizer=sgd,
-                         loss='categorical_crossentropy')
+# Building deep neural network: N layers, fully connected
+net = tflearn.input_data(shape=[None, 32])
+net = tflearn.fully_connected(net, 400, activation='relu', regularizer='L2')
+net = tflearn.fully_connected(net, 2, activation='relu')
+net = tflearn.regression(net, optimizer='adam')
+#net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
 
 # Training
 model = tflearn.DNN(net, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=20, validation_set=(testX, testY),
-          show_metric=True, run_id="dense_model")
+model.fit(X, Y, n_epoch=20, validation_set=(testX, testY), show_metric=True, run_id="dense_model")
+
